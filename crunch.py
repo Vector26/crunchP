4ea1d0e4ae182f374aed5a247a8e42ede5322b79
vector26 <hritik.26.rai@gmail.com>
2020-11-12 18:51:02 +0530

    incomplete v1.1

--- crunch.py
+++ crunch.py
@@ -1,12 +1,17 @@
-import sys
-import subprocess
 import getopt
+import subprocess
+import sys
+from numba import vectorize, jit,cuda
 
+
+@vectorize(["string(dict)"],target="cuda")
 def dictionaryGen():
     wordlist=list(''.join(load['charlist']))
     #print(wordlist)
     maxi=int(load['maxLim'])
     mini=int(load['minLim'])
+    query=load['shell']
+    #print("query"+query)
     print('#' * 10)
     for i in range(mini,maxi+1):
         temp=len(wordlist)**i
@@ -19,22 +24,22 @@ def dictionaryGen():
                 #print('c:%d,i:%d,k:%d' % (c,index,k))
                 word.append(wordlist[index])
             password=("".join(word))
-            query="hydra -l demo -p %s 192.168.2.62 http-post-form “/dvwa/login.php:username=^USER^&password=^PASS^&Login=Login:Login Failed"
+            #query="hydra -l demo -p %s 192.168.2.62 http-post-form “/dvwa/login.php:username=^USER^&password=^PASS^&Login=Login:Login Failed"
+            #os.system(query.format(Pass=password))
+            print((subprocess.check_output(query.format(Pass=password),shell=True)).decode("utf-8").replace("\r\n",""))
 
-            #subprocess.call(query,shell=True)
-
-            if(password=="tEstin12"):
-                print("\rpassword Found: %s " % (password))
-                break
-            else:
-                print("\rtrying password: %s" % (password), end="")
+            # if(password=="tEstin12"):
+            #     print("\rpassword Found: %s " % (password))
+            #     break
+            # else:
+            #     print("\rtrying password: %s" % (password), end="")
 
 
 load={}
 def arguementInputter():
     Arglist=sys.argv[1:]
-    s_opt="c:h:l:"
-    l_opt=["charlist=","highest=","lowest="]
+    s_opt="c:h:l:s:"
+    l_opt=["charlist=","highest=","lowest=","shell="]
     try:
         args,opts=getopt.getopt(Arglist,s_opt,l_opt)
 
@@ -45,6 +50,8 @@ def arguementInputter():
                 load.update({'maxLim':value})
             elif (key in ('-l', '--lowest')):
                 load.update({'minLim':value})
+            elif (key in ('-s', '--shell')):
+                load.update({'shell':value})
         return load
     except getopt.error as err:
         # output error, and return with an error code
---
+++ readme.md
@@ -0,0 +1,16 @@
+# crunchP
+A custom brute-forcer which doesn't take much storage, instead uses GPU
+
+#Requirements
+<li>Python v3.x</li>
+<li>Pip or Conda (To install modules therefore OPTIONAL)</li>
+<li>GPU with CUDA cores</li>
+<li>CUDA toolkit (<a href="https://developer.nvidia.com/cuda-downloads">Toolkit</a>)</li>
+
+#Installation
+<li>First make sure that Nvidia drivers are upto date also you can install cudatoolkit explicitly from here.then install Anaconda add anaconda to environment while installing.
+After completion of all the installations run the following commands in the command prompt.
+</li>
+<li>`pip3 install numba & pip3 install cudatoolkit`</li>
+
+<li>`pip3 install numba & pip3 install cudatoolkit`</li>
